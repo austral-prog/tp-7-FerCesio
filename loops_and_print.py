2d55f6fe4d208d5265443538c01ad701c83387de
FerCesio <162721896+FerCesio@users.noreply.github.com>
2024-05-22 19:34:45 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,20 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(list = ["Red", "Green", "", "White", "Black"]):
+    new = []
+    a = 0
+    for element in list:
+        if element:
+            new.append(f"{a}. {element}")
+            a = a+1
+    return new
+enumerate_list()
 
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(list = ["Red", "Green", "", "White", "Black"]):
+    new = []
+    a = 0
+    for element in list:
+        if element:
+            new.append(f"{a}. {element[::-1]}")
+            a = a+1
+    return new
+enumerate_backwards()
